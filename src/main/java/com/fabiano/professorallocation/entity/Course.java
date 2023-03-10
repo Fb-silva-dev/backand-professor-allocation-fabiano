38851b4d35c6a2c29c606738e2f72119e6cfbbf2
Fabiano Silva <fb.silva.tec@hotmail.com>
2023-03-10 19:53:28 -0300

    Add Camada de Modelo Course

--- src/main/java/com/fabiano/professorallocation/entity/Course.java
+++ src/main/java/com/fabiano/professorallocation/entity/Course.java
@@ -1,6 +1,46 @@
 package com.fabiano.professorallocation.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "course")
 public class Course {
-private Long id;
-private String name;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(unique = true, nullable = false)
+	private Long id;
+	//@Column(length = 100, nullable = false)
+	private String name;
+
+	public Course() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", name=" + name + "]";
+	}
+
 }
