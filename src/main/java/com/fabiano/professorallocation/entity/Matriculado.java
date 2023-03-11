3e2c3ecaef74fd517a0e797b8e8110ec75824dad
Fabiano Silva <fb.silva.tec@hotmail.com>
2023-03-10 21:17:15 -0300

    Alterando o nome da classe de Modelo "Frequenta para Matriculado"

---
+++ src/main/java/com/fabiano/professorallocation/entity/Matriculado.java
@@ -0,0 +1,79 @@
+package com.fabiano.professorallocation.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "matriculado")
+public class Matriculado {
+	@Id
+	@GeneratedValue(strategy  = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(length = 4, nullable = false)
+	private int ano;
+	
+	@Column(length = 4, nullable = false)
+	private String semestre;
+
+	private Double valor;
+
+	private Long alunomoId;
+
+	private Long courseId;
+
+	public Matriculado() {
+		super();
+	}
+
+	public Long getAlunomoId() {
+		return alunomoId;
+	}
+
+	public void setAlunomoId(Long alunomoId) {
+		this.alunomoId = alunomoId;
+	}
+
+	public Long getCourseId() {
+		return courseId;
+	}
+
+	public void setCourseId(Long courseId) {
+		this.courseId = courseId;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+
+	public String getSemestre() {
+		return semestre;
+	}
+
+	public void setSemestre(String semestre) {
+		this.semestre = semestre;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	@Override
+	public String toString() {
+		return "Frequenta [alunomoId=" + alunomoId + ", courseId=" + courseId + ", ano=" + ano + ", semestre="
+				+ semestre + ", valor=" + valor + "]";
+	}
+
+}
