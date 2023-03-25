4471212c74462a4e8999e66a3c4a380f204ccbf4
Fabiano Silva <fb.silva.tec@hotmail.com>
2023-03-25 19:41:10 -0300

    Ajuste MatriculadoRepository

--- src/main/java/com/fabiano/professorallocation/repository/MatriculaRepository.java
+++ src/main/java/com/fabiano/professorallocation/repository/MatriculaRepository.java
@@ -1,11 +1,14 @@
 package com.fabiano.professorallocation.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.fabiano.professorallocation.entity.Matriculado;
 
 @Repository
 public interface MatriculaRepository extends JpaRepository<Matriculado, Long> {
+	List<Matriculado> findByAlunoId(Long aluno_id);
 
+	List<Matriculado> findByCourseId(Long course_id);
 }
