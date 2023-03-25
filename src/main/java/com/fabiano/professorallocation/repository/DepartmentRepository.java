239a8716d2f54074a4dcb4d19616362c45bbc9c2
Fabiano Silva <fb.silva.tec@hotmail.com>
2023-03-25 19:40:51 -0300

    Ajuste DepartmentRepository

--- src/main/java/com/fabiano/professorallocation/repository/DepartmentRepository.java
+++ src/main/java/com/fabiano/professorallocation/repository/DepartmentRepository.java
@@ -1,11 +1,13 @@
 package com.fabiano.professorallocation.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.fabiano.professorallocation.entity.Department;
 
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
 
+	List<Department> findByNameContainingIgnoreCase(String name);
 }
