c723d102887df9395ec1b0a5313253dd05f04362
Fabiano Silva <fb.silva.tec@hotmail.com>
2023-03-25 19:40:30 -0300

    Ajuste Aluno Repository

--- src/main/java/com/fabiano/professorallocation/repository/AlunoRepository.java
+++ src/main/java/com/fabiano/professorallocation/repository/AlunoRepository.java
@@ -1,11 +1,13 @@
 package com.fabiano.professorallocation.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.fabiano.professorallocation.entity.Aluno;
 
 @Repository
 public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+	List<Aluno> findByResponsavelId(Long responsavelId);
 
 }
