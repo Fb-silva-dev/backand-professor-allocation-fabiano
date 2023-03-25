692e19aa68bf4c16b5596bf5525bc82ab05d2365
Fabiano Silva <fb.silva.tec@hotmail.com>
2023-03-25 19:40:13 -0300

    Ajuste na cama de AllocationRepository

--- src/main/java/com/fabiano/professorallocation/repository/AllocationRepository.java
+++ src/main/java/com/fabiano/professorallocation/repository/AllocationRepository.java
@@ -1,10 +1,16 @@
 package com.fabiano.professorallocation.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.fabiano.professorallocation.entity.Allocation;
+import com.fabiano.professorallocation.entity.Course;
 
 @Repository
 public interface AllocationRepository extends JpaRepository<Allocation, Long> {
+	List<Allocation> findByProfessorId(Long professor_id);
+
+	List<Course> findByCourseId(Long course_id);
 
 }
