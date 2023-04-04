905bfe81114e4c66e7efbd5ac7849077c83c9830
Fabiano Silva <fb.silva.tec@hotmail.com>
2023-04-04 16:29:19 -0300

    Ajuste Repositorio Course Repository

--- src/main/java/com/fabiano/professorallocation/repository/CourseRepository.java
+++ src/main/java/com/fabiano/professorallocation/repository/CourseRepository.java
@@ -1,11 +1,13 @@
 package com.fabiano.professorallocation.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.fabiano.professorallocation.entity.Course;
+import com.fabiano.professorallocation.entity.Matricula;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
-
+List<Matricula>findByMatricula(Long matriculaId);
 }
