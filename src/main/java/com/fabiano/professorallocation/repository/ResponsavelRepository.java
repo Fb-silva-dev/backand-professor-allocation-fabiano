fc32dd4051c1e03420e8f151b4bcfae9a253ba8d
Fabiano Silva <fb.silva.tec@hotmail.com>
2023-04-07 01:15:58 -0300

    Ajuste na camada de Repositorio responsavel

--- src/main/java/com/fabiano/professorallocation/repository/ResponsavelRepository.java
+++ src/main/java/com/fabiano/professorallocation/repository/ResponsavelRepository.java
@@ -1,11 +1,11 @@
 package com.fabiano.professorallocation.repository;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.fabiano.professorallocation.entity.Responsavel;
 
 @Repository
 public interface ResponsavelRepository extends JpaRepository<Responsavel, Long> {
-
 
 }
