29288c96d3a1c354e965ff9597a060bb8cf5a9db
Fabiano Silva <fb.silva.tec@hotmail.com>
2023-04-09 14:40:00 -0300

    update

--- src/main/java/com/fabiano/professorallocation/service/AlunoService.java
+++ src/main/java/com/fabiano/professorallocation/service/AlunoService.java
@@ -3,21 +3,17 @@ package com.fabiano.professorallocation.service;
 import org.springframework.stereotype.Service;
 
 import com.fabiano.professorallocation.entity.Aluno;
-import com.fabiano.professorallocation.entity.Course;
 import com.fabiano.professorallocation.repository.AlunoRepository;
 
 @Service
 public class AlunoService {
 
 private final AlunoRepository repository;
+
 public AlunoService(AlunoRepository repository) {
 	super();
 	this.repository = repository;
 }
 
-public Aluno create(Aluno aluno) {
-	// uma criação so pode ser feita quando não exisxtir um id
-	aluno.setId(null);
-	return saveInternal(aluno);
-}
+
 }
